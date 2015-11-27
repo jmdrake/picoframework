7a9bc9189bea9907651735b991edf96bd977a7ff
Snipes901 <steven@godigio.com>
2015-11-27 17:06:18 +0000

    added getPost.php

---
+++ controllers/getPost.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * File : 
+ * Input type: GET
+ * Inputs: 
+ * Outputs: 
+ */
+
+require "../php/querytojson.php";
+require "../php/config.php";
+require "../php/mark_sql_get.php";
+
+$conn = open_connection();
+$sql = mark_sql_get("SELECT id, text, sharedpost, image FROM Posts WHERE user = [user]");
+echo querytojson($sql, $conn);
+$conn->close();
+?>
