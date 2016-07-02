309549f91c14e293c0a6c48c768447f734d7402e
John <johnmdrake@gmail.com>
2016-07-02 07:30:49 -0500

    change to login.php

--- controllers/login.php
+++ controllers/login.php
@@ -1,32 +1,26 @@
 <?php
 /*
- * File : getprofiledata.php
- * Input type: GET
- * Inputs: user
- * Outputs: Returns name and image fields from user profile
+ * File : login.php
+ * Input type: POST
+ * Inputs: username, password
+ * Outputs: Returns currentuser
  */
-
 
 require "../php/querytojson.php";
 require "../php/config.php";
 require "../php/mark_sql_post.php";
 
 $conn = open_connection();
-
 $sql = mark_sql_post("SELECT id FROM Users WHERE email=[email] AND password=[password]");
-
-
 $result = $conn->query($sql);
 if($result) {
     $rs = $result->fetch_array(MYSQLI_ASSOC);
     $currentuser = $rs["id"];
-    if($currentuser !== "")
-        
+    if($currentuser !== "")        
        setcookie("currentuser", $currentuser);
     echo $currentuser;
 } else {
     echo("Unable to fetch login record");
 }
-
 $conn->close();
 ?>
