fba7692d8b2c002fb92eba4dc66e639195c8ef41
jmdrake <johnmdrake@gmail.com>
2021-10-22 11:11:52 -0500

    Create new config file
    Added new config file for Infinity Free hosting

---
+++ php/config_infinity.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "sql110.epizy.com";
+$username = "epiz_30071695";
+$password = "5JzYWbjwlV7JFeC";
+$database = "epiz_30071695_fanhub";
+
+$smtphost = '';  // Specify main and backup SMTP servers
+$smtpauth = true;                               // Enable SMTP authentication
+$smtpusername = '';
+$smtppassword = '';                           // SMTP password
+$smtpport = 25;
+$replyemail = "";
+$replyname = "";
+
+$baseurl = "http://jmdportfolio.epizy.com/fanhub-master/";
+?>
