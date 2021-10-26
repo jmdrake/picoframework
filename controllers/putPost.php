ed12f31ce8c53db6cb1896935764f33a845a6778
jmdrake <johnmdrake@gmail.com>
2021-10-26 15:58:51 -0500

    Update putPost.php

--- controllers/putPost.php
+++ controllers/putPost.php
@@ -16,11 +16,14 @@ $timestamp = time();
 $sql = mark_sql_post("INSERT INTO Posts(text, user, image, audio, video, tags, timestamp) 
 VALUES ([text], [currentuser], [image], [audio], [video], [tags], '" . $timestamp . "');");
 
-$results = $conn->query($sql);    
+if ($conn->query($sql) === TRUE) {
+  $selquery = mark_sql_post("SELECT text, Posts.id AS postid, Users.id AS userid, userimage, Posts.image AS image, audio, video, tags 
+  FROM Posts INNER JOIN Users ON Posts.user = Users.id WHERE Posts.user = [currentuser] AND Posts.timestamp = " . $timestamp);
 
-$selquery = mark_sql_post("SELECT text, Posts.id AS postid, Users.id AS userid, userimage, Posts.image AS image, audio, video, tags 
-FROM Posts INNER JOIN Users ON Posts.user = Users.id WHERE Posts.user = [currentuser] AND Posts.timestamp = " . $timestamp);
+  echo querytojson($selquery, $conn);
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-echo querytojson($selquery, $conn);
 $conn->close();
 ?>
